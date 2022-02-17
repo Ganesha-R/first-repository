9b48d50e69a0ef1ee3e783859ba54e88c1856b69
athrayaa <rganesh969@gmail.com>
2022-02-17 18:15:06 +0530

    first commit

---
+++ pass.py
@@ -0,0 +1,12 @@
+import random
+
+upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower="abcdefghijklmnopqrstuvwxyz"
+number="0123456789"
+symbol="!@#$%^&*()_"
+
+string= upper+lower+number+symbol
+length=16
+
+password="".join(random.sample(string,length))
+print("your new password is: "+password)
